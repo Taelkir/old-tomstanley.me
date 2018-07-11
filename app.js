2d886ea58424ecb3a31def6a4947075e2d5011fc
Tom <tom.stanley37@gmail.com>
2018-07-11 21:30:47 +0100

    grunt, sass, pug set up

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+const port = 3000 || "";
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -39,3 +41,5 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+// app.listen(port, () => console.log("Running!"));
