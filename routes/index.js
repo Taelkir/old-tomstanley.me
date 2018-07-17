2de35d2a9825c64f7362dd9995f07b603ab8acf6
Tom <tom.stanley37@gmail.com>
2018-07-17 19:04:11 +0100

    bare bones page customisation

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Tom Stanley - Web Developer' });
 });
 
+/* POST home page. */
+router.post('/', function(req, res, next) {
+  res.render('index', { title: 'Tom Stanley - Web Developer', name: req.body.name });
+});
+
+
 module.exports = router;
